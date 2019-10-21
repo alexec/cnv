f15060e16f373a9e5787559be1287feb6ee7a355
Alex Collins <alex_collins@intuit.com>
2019-10-21 14:48:07 -0700

    changes on master to package.json,src/components/Converter.jsx,src/index.html,src/index.jsx,webpack.config.js,yarn.lock,

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld';
-import './styles/app.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-ReactDOM.render(
-   <HelloWorld />,
-   document.getElementById('app')
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import Converter from "./components/Converter";
+import "./styles/app.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(<Converter />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,44 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+const path = require("path");
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
-    devServer: {
-        contentBase: path.join(__dirname, 'dist')
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.scss/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ]
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/index.html",
-        filename: "./index.html"
-    })]
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  devServer: {
+    contentBase: path.join(__dirname, "dist")
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MonacoWebpackPlugin({
+      // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+      languages: ["json", "yaml"]
+    })
+  ]
 };
