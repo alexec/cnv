2263c0d259bb958d80fac5e14d9357441ea65928
Alex Collins <alex_collins@intuit.com>
2019-11-04 13:08:10 -0800

    changes on master to

--- src/components/hex.test.js
+++ src/components/hex.test.js
@@ -1,9 +1,9 @@
-import {hexDecode, hexEncode} from "./hex";
+import { hexDecode, hexEncode } from "./hex";
 
 test("hexEncode", () => {
-    expect(hexEncode("foo")).toBe("666f6f");
+  expect(hexEncode("foo")).toBe("666f6f");
 });
 
 test("hexDecode", () => {
-    expect(hexDecode("666f6f")).toBe("foo");
+  expect(hexDecode("666f6f")).toBe("foo");
 });
